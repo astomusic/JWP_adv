ee0dfda78fb44e607ce12d4e63db5ec70ede6ecf
astomusic <astomusic@naver.com>
2014-07-10 19:07:28 +0900

    log4j setting

--- src/main/java/net/asto/web/HomeController.java
+++ src/main/java/net/asto/web/HomeController.java
@@ -1,12 +1,16 @@
 package net.asto.web;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	public static Logger logger = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String home() {
+		logger.info("test");
 		// /home.jps (prefix + name + suffix)
 		return "home";
 	}
